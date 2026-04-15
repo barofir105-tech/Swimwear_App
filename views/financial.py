b49adddf8f2be29d9036540c3c7874933cd6a075
Bar Ofir <barofir105@gmail.com>
2026-04-15 22:18:07 +0300

    TBF3

--- views/financial.py
+++ views/financial.py
@@ -44,12 +44,6 @@ def render_financial():
                     pass
             save_finance_data(finance_data)
 
-        # One-time cleanup of legacy automated transactions (Phase 2 Clean-up)
-        if "transactions" in finance_data:
-            initial_count = len(finance_data["transactions"])
-            finance_data["transactions"] = [t for t in finance_data["transactions"] if not t.get("is_automated", False)]
-            if len(finance_data["transactions"]) < initial_count:
-                save_finance_data(finance_data)
 
 
 
